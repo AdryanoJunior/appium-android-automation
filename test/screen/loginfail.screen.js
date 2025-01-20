d679ccdaf5c2d94a93081eeb3f67548bf00787db
Adryano Junior <jusamsung10@gmail.com>
2025-01-20 19:19:22 -0300

    putting dynamic data into automation

--- test/screen/loginfail.screen.js
+++ test/screen/loginfail.screen.js
@@ -1,16 +1,16 @@
-class LoginScreen {
+class LoginFailScreen {
     constructor() {
         this.usernameField = "~Username input field"
         this.passwordField = "~Password input field"
         this.loginButton = "~Login button"
-        
+        this.errorMessage = "//android.view.ViewGroup[@content-desc='generic-error-message']/android.widget.TextView"
     }
-    async loginWithValidUser(username, password) {
+    async loginWithInvalidUser(username, password) {
         await $(this.usernameField).setValue(username)
         await $(this.passwordField).setValue(password)
         await $(this.loginButton).click()
-       
+        await $(this.errorMessage)
     }
 }
 
-export default new LoginScreen()
+export default new LoginFailScreen()
--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -1,16 +1,30 @@
+import userData from '../data/userData.json'
 import { expect } from '@wdio/globals'
 import { $ } from '@wdio/globals'
 import MenuScreen from '../screen/menu.screen.js'
+import LoginFailScreen from '../screen/loginfail.screen.js'
 import LoginScreen from '../screen/login.screen.js'
 
 describe('My Login application', () => {
     it('should login with invalid credentials', async () => {
        await MenuScreen.accessLoginScreen()
-       await LoginScreen.loginWithInvalidUser()
-       await expect($(LoginScreen.errorMessage).toHaveText("Provided credentials do not match any user in this service."))
+       await LoginFailScreen.loginWithInvalidUser(userData.userFail.username, userData.userFail.password)
+       await expect($(LoginFailScreen.errorMessage).toHaveText("Provided credentials do not match any user in this service."))
     })
+
+    it('should login with valid credentials', async () => {
+      await MenuScreen.accessLoginScreen()
+      await LoginScreen.loginWithValidUser(userData.userSuccess.username, userData.userSuccess.password)
+     
+   })
+
 
 
 })
+
+ 
+ 
+
+
 
 
